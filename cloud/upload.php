32adb78487176c692c7f85ba4bdd3760646cf3fe
Bradley Smith <s3318889@student.rmit.edu.au>
2018-10-09 05:02:09 +1100

    Upload to gcloud storage bucket

--- cloud/upload.php
+++ cloud/upload.php
@@ -1,4 +1,8 @@
 <?php
-use google\appengine\api\cloud_storage\CloudStorageTools;
+require_once('env.php');
+$gs_name = $_FILES['convertFile']['tmp_name'];
+$id = uniqid();
+move_uploaded_file($gs_name, "gs://" . $bucket_name . "/" . $id . ".txt");
 var_dump($_FILES);
+echo $id;
 ?>
